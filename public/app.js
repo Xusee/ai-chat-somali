41212c5513ed67b41ea734ff78b38de1bd0a50de
Xusee <108761804+Xusee@users.noreply.github.com>
2026-09-20 16:43:36 +0300

    Revise welcome message and question prompt
    Updated text for clarity and corrected title format.

--- public/app.js
+++ public/app.js
@@ -1565,14 +1565,14 @@
 
       <h2>
         Ku soo dhawoow
-        NASIIB BUSINESS CENTER <abbr titale="DR maxamuud bodhari">DMB</abbr>
+        NASIIB BUSINESS CENTER DR maxamuud (bodhari)
       </h2>
 
       <p>
-        Weydii su'aal Af Soomaali ah.
+        Weydii su'aal.
         Waxaad diri kartaa qoraal,
-        sawir ,<abbr titale="fadlan sawirku ha ahaado inta cad oo kaliya ka jar 
-          hadii aad screenshot ku qaaday ka jar hareeraha">fadlan</abbr> .
+        sawir, fadlan sawirku ha ahaado inta cad oo kaliya ka jar 
+          hadii aad screenshot ku qaaday ka jar hareeraha.
       </p>
     `;
 
